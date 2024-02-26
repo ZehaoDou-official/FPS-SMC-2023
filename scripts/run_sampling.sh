671593dcb5b00f423edd019b00a8868e8b5100c2
Zehao Dou <90218938+ZehaoDou-official@users.noreply.github.com>
2024-02-26 15:33:17 -0500

    Update run_sampling.sh

--- scripts/run_sampling.sh
+++ scripts/run_sampling.sh
@@ -2,10 +2,15 @@
 
 # $1: task
 # $2: gpu number
+# $3: output directory
+# $4: c_rate, default 0.95
+# $5: particle_size, default 5
 
 python3 sample_condition.py \
     --model_config=configs/model_config.yaml \
     --diffusion_config=configs/diffusion_config.yaml \
     --task_config=configs/$1_config.yaml \
     --gpu=$2 \
-    --save_dir=$3;
+    --save_dir=$3 \
+    --c_rate=$4 \
+    --particle_size = $5;
